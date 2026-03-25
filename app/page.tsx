22ef0fe78e0a4ff81f4737c9baedd6b174cbf58e
Federica Pitetta <federicapitetta@Federicas-MacBook-Air.local>
2026-03-25 10:49:49 -0300

    pietta web

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold mb-6">Pietta</h1>
+      <p>Muebles para habitar el exterior</p>
+    </main>
+  )
+}
